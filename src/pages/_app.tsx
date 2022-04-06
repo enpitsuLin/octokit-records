6533fdb08d19b38668b2795a249a31457b577c0c
enpitsulin <lldslm@vip.qq.com>
2022-04-06 09:46:24 +0800

    theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,1 +1,17 @@
 import "windi.css";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Component {...pageProps}></Component>
+    </ThemeProvider>
+  );
+};
+
+export default App;
