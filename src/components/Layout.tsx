759edace0e6d3d83e6584832279428b5731e446d
enpitsulin <enpitsulin@gmail.com>
2022-07-08 09:20:01 +0800

    fix: prop type in react 18

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,12 @@
-import Header from "./Header";
+import Header from './Header'
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="transition-all duration-300 mx-10 md:(max-w-150 mx-auto)">
       <Header />
       <div>{children}</div>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
